e19084a8ff55d7d02ded8379f3b620abd20513ac
ArtemOsypov <artem.osypov@gmail.com>
2023-08-17 01:22:59 +0300

    fix email validation

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,12 +1,21 @@
 import User from "../models/user.js";
 import { HttpError } from "../helpers/index.js";
 
-const signup = async (req, res) => {
-  const newUser = await User.create(req.body);
-  res.status(201).json({
-    name: newUser.name,
-    email: newUser.email,
-  });
+const signup = async (req, res, next) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw HttpError(409, "email in use");
+    }
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      name: newUser.name,
+      email: newUser.email,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
